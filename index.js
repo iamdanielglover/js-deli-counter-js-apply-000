8cf8d9aa2ce86842bde0a06f3b4e777c8672b506
Daniel Glover <iamdanielglover@gmail.com>
2019-01-07 17:43:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,46 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+}
+
+function nowServing() {
+  
+}
+
+function currentLine() {
+  
+}
+
+
+
+
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine>[]) {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  } else {
+    return 'There is nobody waiting to be served!';
+  }
+}
+
+function currentLine(katzDeliLine) {
+ var str = "The line is currently:";
+ var line = [];
+ for (var i=0; i < katzDeliLine.length; i++) {
+   line.push(` ${i+1}. ${katzDeliLine[i]}`);
+ }
+ if (katzDeliLine.length === 0) {
+   return "The line is currently empty.";
+ } else {
+   return str += line;
+ }
+}
+
+ 
