27dafb8a7a622e711a4dc732095d06f045536aae
Daniel Glover <iamdanielglover@gmail.com>
2019-01-07 17:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(katzDeliLine, name) {
   return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   
 }
 
